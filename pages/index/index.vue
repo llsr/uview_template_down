ced7be7dc69b544bd305bc6690fe4a4b9a5cda6d
llsr <hklangwang@126.com>
2021-10-27 11:21:52 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,23 @@
+<template>
+	<view>
+		<view>
+			版本号为：{{vuex_version}}
+		</view>
+		<view>
+			<<琵琶行>>的作者为{{vuex_user.name}}
+		</view>
+		<u-button type="primary" @click="modifyVuex">修改变量</u-button>
+	</view>
+</template>
+
+<script>
+	export default {
+		methods: {
+			modifyVuex() {
+				this.$u.vuex('vuex_version', '1.0.1');
+				// 修改对象的形式，中间用"."分隔
+				this.$u.vuex('vuex_user.name', '诗圣');
+			}
+		}
+	}
+</script>
---
+++ pages/login/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<u-button @click="sumbitByPost" type="primary">发送登陆验证码</u-button>
+	</view>
+</template>
+
+<script>
+	export default {
+		methods: {
+			// post示例
+			sumbitByPost() {
+				this.$u.api.sendLoginCode({phone: '17865288896'}).then(res => {
+					console.log(res);
+				})
+			}
+			
+		}
+	}
+</script>
+
+<style>
+</style>
